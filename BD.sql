377d71f73c640e1587ffad03d07a87513330047a
Guilherme Heitor <162808251+GuilhermeHeitorB@users.noreply.github.com>
2024-11-18 19:48:29 -0300

    Add files via upload

--- BD.sql
+++ BD.sql
@@ -1,17 +1,20 @@
 CREATE TABLE usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     usu_nome VARCHAR(100) NOT NULL,
-    usu_email VARCHAR(100) UNIQUE,
-    usu_password VARCHAR(100) UNIQUE
+    usu_email VARCHAR(100) UNIQUE PRIMARY KEY,
+    usu_password VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE agendamentos (
     id_agendamento INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    data_hora DATETIME NOT NULL,
     numero_pessoas INT NOT NULL,
+    data_ag DATE NOT NULL,
+    hora_ag TIME NOT NULL,
+    nome_ag VARCHAR(50) NOT NULL,
+    usu_email VARCHAR(100) NOT NULL,
     observacoes TEXT,
     status ENUM('PENDENTE', 'CONFIRMADO', 'CANCELADO') DEFAULT 'PENDENTE',
-    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (usu_email) REFERENCES usuarios(usu_email)
 );
+
+SELECT * FROM usuarios;
+SELECT * FROM agendamentos
